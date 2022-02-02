3d3124b8ad06518e3799c54bde75c2d57ef8e04c
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-02-01 19:23:45 -0600

    bug with env file

---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+//Collect all of the API routes and package them 
+
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes.js');
+
+router.use('/users', userRoutes);
+
+module.exports = router;
