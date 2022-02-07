1a7170f6039cd190c9389bcb86dca1d8358fb704
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-02-07 06:38:25 -0600

    adding the style.css to our page

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
@@ -5,6 +6,7 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -13,6 +15,6 @@ app.use(routes);
 
 //turn on connection to db and server The "sync" part means that this is Sequelize taking the models and connecting them to associated database tables. If it doesn't find a table, it'll create it for you!
 //By forcing the sync method to true, we will make the tables re-create if there are any association changes.
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening!'));
 });
